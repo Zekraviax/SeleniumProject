c8ef9d6a96e78260d16d9d5a63d829adc513f0b9
Valerie Clarke <Valerie.Clarke@fdmgroup.com>
2022-07-04 15:55:04 +1000

    Begun attempt to ingertate specflow

---
+++ SeleniumProject/FeatureFiles/Purchase.feature
@@ -0,0 +1,18 @@
+﻿#Feature: SpecFlowFeature1
+#	Simple calculator for adding two numbers
+#
+#@mytag
+#Scenario: Add two numbers
+#	Given the first number is 50
+#	And the second number is 70
+#	When the two numbers are added
+#	Then the result should be 120
+
+
+Feature: Purchase Test
+	Create an automated test that purchases an item from the AutomationPractice website.
+
+@purchase
+Scenario: Purchase an item
+	Given a user is signed in to the AutomationPractice website with email "jane_smith222@gmail.com" and password "averysecurepassword?123"
+	And the user is on the home page
---
+++ SeleniumProject/StepDefinitionFiles/PurchaseStepDefinition.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SeleniumProject.StepDefinitionFiles
+{
+    [Binding]
+    public sealed class PurchaseStepDefinition
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public PurchaseStepDefinition(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeTestRun]
+        public void Setup()
+        {
+            //PicoContainer 
+        }
+
+
+        // Defaults
+        //[Given("the first number is (.*)")]
+        //public void GivenTheFirstNumberIs(int number)
+        //{
+        //    //TODO: implement arrange (precondition) logic
+        //    // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        //    // To use the multiline text or the table argument of the scenario,
+        //    // additional string/Table parameters can be defined on the step definition
+        //    // method. 
+
+        //    _scenarioContext.Pending();
+        //}
+
+        //[Given("the second number is (.*)")]
+        //public void GivenTheSecondNumberIs(int number)
+        //{
+        //    //TODO: implement arrange (precondition) logic
+        //    // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        //    // To use the multiline text or the table argument of the scenario,
+        //    // additional string/Table parameters can be defined on the step definition
+        //    // method. 
+
+        //    _scenarioContext.Pending();
+        //}
+
+        //[When("the two numbers are added")]
+        //public void WhenTheTwoNumbersAreAdded()
+        //{
+        //    //TODO: implement act (action) logic
+
+        //    _scenarioContext.Pending();
+        //}
+
+        //[Then("the result should be (.*)")]
+        //public void ThenTheResultShouldBe(int result)
+        //{
+        //    //TODO: implement assert (verification) logic
+
+        //    _scenarioContext.Pending();
+        //}
+
+
+        // Purchase test
+        [Given("a user is signed in to the AutomationPractice website with email (.*) and password (.*)")]
+        public void GivenAUserIsSignedIntoTheAutomationPracticeWebsite(string email, string password)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Pending();
+        }
+    }
+}
